952836ad686a9b3c85fd8aaca3ec858e6e794d84
Michal <c3022545@newcastle.ac.uk>
2024-02-19 14:23:00 +0000

    Completed longestFilm and averageEarnings methods

--- src/FilmStudio.java
+++ src/FilmStudio.java
@@ -5,13 +5,41 @@ public class FilmStudio {
     String studio_name;
     List<Film> films;
 
-    public FilmStudio(String studio_name, List<Film> films) {
+    public FilmStudio(String studio_name) {
         this.studio_name = studio_name;
         this.films = new ArrayList<>();
     }
+    public void addFilm(Film film) {
+        films.add(film);
+    }
+    public Film longestFilm() {
+        Film longest_film = null;
+        int longest = 0;
+        for (Film film : films) {
+            if (film.getLength() > longest) {
+                longest = film.getLength();
+                longest_film = film;
+            }
+        }
+        return longest_film;
+    }
+    public float averageEarnings(int year) {
+        float total = 0;
+        int count = 0;
+        for (Film film : films) {
+            if (film.getRelease_year() == year) {
+                total = total + film.getEarnings();
+                count = count + 1;
+            }
+        }
+        return total / count;
+    }
+    public List filmsLessThan() {
+
+    }
 
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         FilmStudio studio = new FilmStudio("Marvel");
         Film film = new Film("pacific", 2020, 90, 1300, "action");
                 System.out.println(film.getLength());
